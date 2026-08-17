449931ed8db1ac61e7e61a41eb85bded0d933716
SirBusayoAmusan <synccityng@gmail.com>
2026-08-17 17:20:32 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // Replace GAS_URL with your deployed Google Apps Script Web App /exec URL.
 // See google-apps-script/Code.gs and the README for setup instructions.
 window.SCALEBRIDGE_CONFIG = {
-  GAS_URL: "REPLACE_WITH_YOUR_APPS_SCRIPT_WEB_APP_URL"
+    GAS_URL: "https://script.google.com/macros/s/AKfycbyCumXHICZXdMD92motRtvuRFkhR06kHw8EPxmwnFiinVYVeFYivLaoZ9GeyEpLQGe6Nw/exec"
 };
